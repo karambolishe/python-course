031f9f6354777f2ca264af42976f07ffa8f212c5
Mykola Novikov <karambolishe@gmail.com>
2015-03-06 17:37:49 +0200

    Add script dictionary

--- Tyschenko/task_3/Dictionary.py
+++ Tyschenko/task_3/Dictionary.py
@@ -1,18 +1,32 @@
 # -*- coding: utf-8 -*-
-dict_en = {}
+d_en_lat = {}
+d_lat_en = {}
 
 from words_for_dict import string
 
-#print('Входные даынне:\n' + string)
+def printing(dict):
+    for key in sorted(dict):
+        print key.ljust(8) + ' - ' + ', '.join(dict[key])
 
 for src_string in string.split('\n'):
-    on_part = src_string.split('-')
-    key = on_part[0].strip(' ')
-    value_src = on_part[1].strip(' ')
-    for val in value_src.split(','):
-        print(key)
-        print(val)
-        print('\n')
-        #dict_en.update({key:(val.strip(' '))})
+    devided_on_part = src_string.split('-')
+    key = devided_on_part[0].strip(' ')
+    value_src = devided_on_part[1].strip(' ')
+    value_lat = [i.strip(' ') for i in str.split(value_src, ',')]
 
-#print(dict_en)
+    d_en_lat[key] = value_lat
+
+for key in d_en_lat:
+    value_en_list = d_en_lat[key]
+    for value_en in value_en_list:
+        if value_en not in d_lat_en:
+            d_lat_en[value_en] = [key]
+        else:
+            d_lat_en[value_en].append(key)
+
+#printing(d_lat_en)
+
+print 'Входные даынне:\n' + str(len(d_en_lat))
+printing(d_en_lat)
+print 'Выходные даынне:\n' + str(len(d_lat_en))
+printing(d_lat_en)
